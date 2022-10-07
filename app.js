caa84f69933ebc29351fd7489c8f0d3a454faa20
Ti-Max <maxtimofy@gmail.com>
2022-10-07 14:13:57 +0200

    .env file and test mysql database connection

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('path');
+const con = require('./dbConnection.js');
+
 
 // Routes
 const indexRouter = require('./routes/index');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ dbConnection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "epictimer"
+});
+
+con.connect(function(error){
+  if(!!error){
+    console.log(error);
+  }else{
+    console.log('Connected to mysql database');
+  }
+});
+
+let sess;
+
+module.exports = con;
