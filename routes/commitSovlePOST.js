9b67768c77872d0e5805332b2ba6af7afcdb19b1
Ti-Max <maxtimofy@gmail.com>
2022-11-16 20:27:08 +0100

    Sort code in files & refactoring

--- routes/commitSovlePOST.js
+++ routes/commitSovlePOST.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const insertSolve = require("./access/userActionsDB.js").insertSolve;
 const getLastSolves = require("./access/userActionsDB.js").getLastSolves;
--- routes/deleteSolvesPOST.js
+++ routes/deleteSolvesPOST.js
@@ -1,14 +1,8 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
+const { updateAverage } = require("./updateAverage.js");
 const deleteSolves = require("./access/userActionsDB.js").deleteSolves;
-const getLastSolves = require("./access/userActionsDB.js").getLastSolves;
-const getSolvesSince = require("./access/userActionsDB.js").getSolvesSince;
-const getSolvesBeforeAmount =
-  require("./access/userActionsDB.js").getSolvesBeforeAmount;
-const updateAO = require("./access/userActionsDB.js").updateAO;
-
-const calculateAO = require("./utils.js").calculateAO;
 
 router.post("/deleteSolves", async function (req, res, next) {
   // Check request
@@ -20,51 +14,7 @@ router.post("/deleteSolves", async function (req, res, next) {
   await deleteSolves(req.user_id, req.body.solves);
 
   // Update ao5 and ao12 for every solve that was created before the deleted solves
-  // Find earliest one
-  const earliestSolve = Math.min(...req.body.solves);
-
-  // Solve ids for those who needs update
-  const solvesToUpdate = await getSolvesSince(
-    req.user_id,
-    req.body.category,
-    earliestSolve
-  );
-
-  const lastAOs = {};
-  // Update
-  for (let i = 0; i < solvesToUpdate.length; i++) {
-    //get 12 solves to calculate ao5 and ao12 before
-    const solves = await getSolvesBeforeAmount(
-      req.user_id,
-      req.body.category,
-      solvesToUpdate[i].id,
-      12
-    );
-    const solvesParsed = [];
-    solves.forEach((solve) => {
-      solvesParsed.push(solve.time);
-    });
-
-    // Calculate average
-    const ao12 = calculateAO(12, [...solvesParsed]); // copy array
-    const ao5 = calculateAO(5, solvesParsed);
-
-    // Insert new ao5 and ao12 to the database
-    await updateAO(solvesToUpdate[i].id, ao5, ao12);
-
-    if (lastAOs.ao5 === undefined) {
-      lastAOs.ao5 = ao5;
-      lastAOs.ao12 = ao12;
-    }
-  }
-
-  if (lastAOs.ao5 === undefined){
-    const lastNewSolve = await getLastSolves(req.user_id, req.body.category, 1);
-    if (lastNewSolve.length !== 0){
-      lastAOs.ao5 = lastNewSolve[0].ao5;
-      lastAOs.ao12 = lastNewSolve[0].ao12;
-    }
-  }
+  const lastAOs = await updateAverage(req.body.solves, req.user_id, req.body.category);
 
   return res.status(201).json({ ao5: lastAOs.ao5, ao12: lastAOs.ao12 });
 });
